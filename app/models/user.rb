468d8bb12b7efbdf284a17f31da218442481ceaa
zhouhang <2011zhouhang@gmail.com>
2014-02-10 13:26:49 +0000

    business set default

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ActiveRecord::Base
   belongs_to :company#,inverse_of: :user
   belongs_to :staff
   accepts_nested_attributes_for  :company
+  before_save :set_default_business_password
 
   validates :name,:staff_id, :presence => true
   #validates :email, :presence => true, :uniqueness => {:case_sensitive => false}, :format => {:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/}
@@ -31,6 +32,12 @@ class User < ActiveRecord::Base
   def staff_valid
     if staff_id.blank? ||Staff.find_by_id(staff_id).blank?
       errors.add(:staff_id, I18n.t('errors.messages.is_not_match'))
+    end
+  end
+
+  def set_default_business_password
+    if self.business_password.blank?
+      self.business_password = '666666'
     end
   end
 
