9d4bbd38c4a7dc689a1410492c28b2b8c2272e4f
acaby <acaby@163.com>
2014-03-17 15:53:30 +0800

    nicom bss login

--- lib/liantong.rb
+++ lib/liantong.rb
@@ -1,5 +1,24 @@
 class Liantong
-  def query(number)
-    return  { name:'aaa',balance:'100' }
+
+  LOGIN_URL = 'https://218.106.115.32/por/login_psw.csp?rnd=gpccmlmgaeoigpae'
+
+  def tel_recharge(params={})
+    uri = URI(LOGIN_URL)
+
+    @https = Net::HTTP.new(uri.host, uri.port)
+    @https.use_ssl = true
+    @https.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    uri.query = URI.encode_www_form(Rails.configuration.liantong_login_params)
+    req = Net::HTTP::Post.new(uri.request_uri)
+    res = @https.request(req)
+    uri = URI("https://218.106.115.32/por/#{res['location']}")
+    req = Net::HTTP::Get.new(uri.request_uri)
+    res = @https.request(req)
+    result = res.body.force_encoding("gb2312").encode!('utf-8')
+    p result
+    result
   end
+
+end
 end
